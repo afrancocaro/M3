79d7c3de4221a7802208fa14da3f5cc27da722d7
Néfix Estrada <nefixestrada@gmail.com>
2017-04-03 13:28:26 +0200

    2017-04-03

---
+++ python/2017-04-03/ejercicio-pedra-paper-tisores-2.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+
+num = 31
+sortir = False
+
+while (sortir == False):
+
+    player1 = num % 7
+    player2 = num % 5
+
+    # Player 1
+    if (player1 == 0) or (player1 == 1):
+        player1 == "tisores"
+    elif (player1 == 2) or (player1 == 3) or (player1 == 6):
+        player1 = "pedra"
+    elif (player1 == 4) or (player1 == 5):
+        player1 = "paper"
+
+    # Player 2
+    if (player2 == 0) or (player2 == 1) or (player2 == 2):
+        player2 = "paper"
+    elif (player2 == 3):
+        player2 = "tisores"
+    elif (player2 == 4):
+        player2 = "pedra"
+
+    # Who wins
+    if (player1 == player2):
+        print "Empat"
+    elif ((player1 == "pedra" and player2 == "tisores") or (player1 == "paper" and player2 == "pedra") or (player1 == "tisores" and player2 == "paper")):
+        print "Player 1 wins"
+    else:
+        print "Player 2 wins"
+
+    if (num == 57):
+        sortir = True
+
+    num = num + 1
---
+++ python/2017-04-03/ejercicio-pedra-paper-tisores-3.1(no empat).py
@@ -0,0 +1,49 @@
+# coding: utf-8
+
+# Imports
+from random import randint
+import os
+
+# Clear
+os.system('clear')
+
+# Vars
+num = 1
+sortir = False
+player1wins = 0
+player2wins = 0
+empats = 0
+
+# Loop
+while (sortir == False):
+
+    # Player 1
+    player1 = randint(0,2)
+
+    # Player 2
+    player2 = randint(0,2)
+
+    # Who wins
+    if (player1 == player2):
+        print "Empat"
+        empats = empats + 1
+        num = num - 1
+
+    elif ((player1 == 0 and player2 == 2) or (player1 == 1 and player2 == 0) or (player1 == 2 and player2 == 1)):
+        print "Player 1 wins"
+        player1wins = player1wins + 1
+    else:
+        print "Player 2 wins"
+        player2wins = player2wins + 1
+
+    if (num == 10):
+        sortir = True
+
+    num = num + 1
+--
+print "-----------------"
+print "- ESTADÍSTIQUES -"
+print "-----------------"
+print "El jugador 1 ha guanyat un " + str(player1wins * 10) + "% de les partides"
+print "El jugador 2 ha guanyat un " + str(player2wins * 10) + "% de les partides"
+print "Hi ha hagut " + str(empats) + " empats"
