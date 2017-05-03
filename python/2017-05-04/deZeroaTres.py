308e5f159e480728f0156ff9ef996cdbf9730c1a
Ulric <ulric@PUTOLINUXNOFUNCIONA>
2017-05-03 17:58:02 +0200

    2017-05-04

---
+++ python/2017-05-04/deZeroaTres.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+sortir = False
+count = 0
+
+while (sortir == False):
+
+    print count
+
+    if (count == 3):
+        sortir = True
+
+    count = count + 1
---
+++ python/2017-05-04/funcParamsOpcionals.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+# Definim la funció
+def dividir(divident = 10, divisor = 2):
+    return divident / divisor
+
+
+print "Sense paràmetres"
+print dividir()
+
+print "Amb 1 paràmetre"
+print dividir(50)
+
+print "Amb 2 paràmetres"
+print dividir(50, 5)
